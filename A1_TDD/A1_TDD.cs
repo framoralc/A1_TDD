a50dc9f3742e19dfdb9fc5629465c5bb36654a8b
framoralc <framoralc@alu.edu.gva.es>
2025-10-22 19:28:39 +0200

    Tests de la funcinalidad CalcularFactorial() hechos

---
+++ A1_TDD/A1_TDD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1_TDD
+{
+    public class A1_TDD
+    {
+    }
+}
---
+++ TestA1_TDD/Test1.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace TestA1_TDD
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void Devuelve_Menos_Uno()
+        {
+            int num1 = -5;
+            int resul = A1_TDD.CalcularFactorial(num1);
+            Assert.AreEqual(-1, resul);
+        }
+
+        [TestMethod]
+        public void Devuelve_Uno()
+        {
+            int num1 = 0;
+            int resul = A1_TDD.CalcularFactorial(num1);
+            Assert.AreEqual(1, resul);
+        }
+
+        [TestMethod]
+        public void = Devuelve_El_Factorial(num1)
+        {
+            int num1 = 5;
+            int resul = A1_TDD.CalcularFactorial(num1);
+            Assert.AreEqual(120, resul);
+        }
+    }
+}
